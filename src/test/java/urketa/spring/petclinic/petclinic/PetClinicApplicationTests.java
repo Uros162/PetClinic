4ed8dffc516a8531931fd9d3cc192752030b213d
Uros162 <ruparuros@gmail.com>
2020-08-05 16:36:35 +0200

    Initial Project Add

---
+++ src/test/java/urketa/spring/petclinic/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package urketa.spring.petclinic.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
