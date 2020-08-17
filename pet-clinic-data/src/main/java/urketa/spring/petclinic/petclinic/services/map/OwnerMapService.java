397a27856321710cbc2a9a3e7166b64425dd9df8
Uros162 <ruparuros@gmail.com>
2020-08-17 14:32:04 +0200

    Imlemented map base service

---
+++ pet-clinic-data/src/main/java/urketa/spring/petclinic/petclinic/services/map/OwnerMapService.java
@@ -0,0 +1,36 @@
+package urketa.spring.petclinic.petclinic.services.map;
+
+import org.graalvm.compiler.lir.StandardOp;
+import urketa.spring.petclinic.petclinic.model.Owner;
+import urketa.spring.petclinic.petclinic.services.CRUDService;
+
+import java.util.Set;
+
+public class OwnerMapService extends AbstractMapService<Owner,Long> implements CRUDService<Owner,Long>{
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(),owner);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+}
