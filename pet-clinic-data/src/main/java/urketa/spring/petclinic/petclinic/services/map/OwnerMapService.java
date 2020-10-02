c8bd25f4a596042bdf734557a75789a88457369d
Uros162 <ruparuros@gmail.com>
2020-10-02 19:32:21 +0200

    updated save method in ownerMapService

--- pet-clinic-data/src/main/java/urketa/spring/petclinic/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/urketa/spring/petclinic/petclinic/services/map/OwnerMapService.java
@@ -3,21 +3,48 @@ package urketa.spring.petclinic.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import urketa.spring.petclinic.petclinic.model.Owner;
-import urketa.spring.petclinic.petclinic.services.CRUDService;
+import urketa.spring.petclinic.petclinic.model.Pet;
 import urketa.spring.petclinic.petclinic.services.OwnerService;
+import urketa.spring.petclinic.petclinic.services.PetService;
+import urketa.spring.petclinic.petclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner,Long> implements OwnerService {
 
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Owner findById(Long id) {
+
+
         return super.findById(id);
     }
 
     @Override
     public Owner save(Owner owner) {
+        if (owner != null){
+            owner.getPets().forEach(pet -> {
+                if (pet.getPetType() != null){
+                    if(pet.getPetType().getId() == null){
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                }else {throw new RuntimeException("Pet type is requred");}
+
+                if (pet.getId() == null){
+                   Pet savedPet = petService.save(pet);
+
+                   pet.setId(savedPet.getId());
+                }
+            });
+        }
         return super.save(owner);
     }
 
--- pet-clinic-web/src/main/java/urketa/spring/petclinic/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/urketa/spring/petclinic/petclinic/bootstrap/DataLoader.java
@@ -3,30 +3,44 @@ package urketa.spring.petclinic.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import urketa.spring.petclinic.petclinic.model.Owner;
+import urketa.spring.petclinic.petclinic.model.PetType;
 import urketa.spring.petclinic.petclinic.model.Vet;
 import urketa.spring.petclinic.petclinic.services.OwnerService;
+import urketa.spring.petclinic.petclinic.services.PetTypeService;
 import urketa.spring.petclinic.petclinic.services.VetService;
-import urketa.spring.petclinic.petclinic.services.map.OwnerMapService;
-import urketa.spring.petclinic.petclinic.services.map.VetMapService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+
+
         Owner owner1  =new Owner();
 
         owner1.setFirstname("Jovandeka");
         owner1.setLastname("Popovic");
+        owner1.setAddress("Milentija Popovica 34");
+        owner1.setCity("Svilajnac");
+        owner1.setTelephone("065 256 25 66");
 
         ownerService.save(owner1);
 
@@ -34,6 +48,9 @@ public class DataLoader implements CommandLineRunner {
 
         owner2.setFirstname("Radenka");
         owner2.setLastname("Gojkovic");
+        owner2.setAddress("Milentija Miliforovica 34");
+        owner2.setCity("Lajkovac");
+        owner2.setTelephone("0645566787");
 
         ownerService.save(owner1);
         ownerService.save(owner2);
