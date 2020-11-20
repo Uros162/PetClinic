4825555ab9f808ccc1d1a5c56c0cccbe1f653cf7
Uros162 <ruparuros@gmail.com>
2020-11-20 18:31:43 +0100

    convert Vet and Specialitiey to JPA

--- pet-clinic-data/src/main/java/urketa/spring/petclinic/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/urketa/spring/petclinic/petclinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package urketa.spring.petclinic.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="speciaities")
 public class Speciality extends BaseEntity{
+
+    @Column(name="decsription")
     private String decsription;
 
     public String getDecsription() {
